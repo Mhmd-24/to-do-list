fd0c9ea1fbd95e886bb729f665539b7034d8a5e8
Mohammad Abbas <mhmd.abbass379@gmail.com>
2025-04-15 15:31:19 +0300

    Modified js
    Added 2 buttons to mark as completed or delete the task with their functionalities using functions

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function addTask() {
   const taskText = task.value.trim();
   const li = document.createElement("li");
   const list = document.getElementById("tasks-list");
+  const completedBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
 
   if (taskText === "") {
     alert("Please enter a task!");
@@ -10,8 +12,22 @@ function addTask() {
   }
 
   li.textContent = taskText;
+  li.appendChild(completedBtn);
+  li.appendChild(deleteBtn);
+
+  completedBtn.innerText = "✔"
+  deleteBtn.innerHTML = "❌"
+
 
   list.appendChild(li);
   task.value = "";
+
+  completedBtn.onclick = () =>{
+    li.classList.toggle("completed");
+  }
+
+  deleteBtn.onclick = () =>{
+    li.remove();
+  }
 
 }
