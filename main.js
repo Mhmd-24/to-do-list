0a7ad6bd33ffca3dc1cae050fb8513b7675578b7
Mohammad Abbas <mhmd.abbass379@gmail.com>
2025-04-18 18:21:37 +0300

    Modified JS
    Added click event listener to check what element has been clicked and to toggle a class in each condition with a span instead of the remove button

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 function addTask() {
+
   const task = document.getElementById("task");
   const taskText = task.value.trim();
-  const li = document.createElement("li");
   const list = document.getElementById("tasks-list");
-  const completedBtn = document.createElement("button");
-  const deleteBtn = document.createElement("button");
-
+  
   if (taskText === "") {
     alert("Please enter a task!");
-    return;
+  }else{
+    const li = document.createElement("li");
+    li.innerHTML = taskText;
+    list.appendChild(li);
+    const remove = document.createElement("span");
+    remove.innerHTML = "&#10006";
+    li.appendChild(remove);
   }
 
-  li.textContent = taskText;
-  li.appendChild(completedBtn);
-  li.appendChild(deleteBtn);
-
-  completedBtn.innerHTML = "&#10003"
-  deleteBtn.innerHTML = "&#10006"
-
-
-  list.appendChild(li);
   task.value = "";
-
-  completedBtn.onclick = () =>{
-    li.classList.toggle("completed");
-  }
-
-  deleteBtn.onclick = () =>{
-    li.remove();
-  }
-
 }
+
+list.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+  }else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove()
+  }
+}, false);
